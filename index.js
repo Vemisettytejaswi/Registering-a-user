66e67ac296f0141e860e9fc2a78b579e11bf39ee
vemisettytejaswi <tejaswisindhu29@gmail.com>
2021-09-01 12:12:17 +0000

    Register and login of user

--- index.js
+++ index.js
@@ -54,3 +54,23 @@ app.post("/users/", async (request, response) => {
     response.send("User Already exists");
   }
 });
+//Login
+app.post("/login/", async (request, response) => {
+  const { username, password } = request.body;
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}';`;
+
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid User");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+
+    if (isPasswordMatched === true) {
+      response.send("Login Success");
+    } else {
+      response.status(400);
+      response.send("Invalid Password");
+    }
+  }
+});
